96d74fdb00d564f138318f55efe878d4ae6ae10b
webwaffle <sam525watchman@gmail.com>
2018-08-19 17:54:17 -0400

    Started new database

---
+++ database.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+module.exports = function(path) {
+  this.path = path;
+  this.queryAll = function() {
+    return new Promise((resolve, reject) => {
+      
+    })
+  }
+}
